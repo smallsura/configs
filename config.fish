b6a0aca961fa418a446213ae90d4e24e0e963a9d
Nicolas Small <smallsura@gmail.com>
2017-12-19 23:07:59 +0100

    Update config.fish

--- config.fish
+++ config.fish
@@ -1,6 +1,9 @@
 alias vim 'nvim'
 alias cvim 'nvim ~/.config/nvim/init.vim'
 
+# so our brew install overrides the commands from the system
+set -x PATH /usr/local/sbin $PATH
+
 # bobthefish theme config 
 set -g theme_nerd_fonts yes
 set -g theme_color_scheme gruvbox
